ac4df5783a1aae198ceac0e9adb56310276a98d7
Joao Cruz <joao.scruz2000@gmail.com>
2020-10-10 21:28:17 +0100

    FIRST COMMMIT TEST

---
+++ test1.c
@@ -0,0 +1,12 @@
+/* programa
+q poe tudo
+em maiusculs */
+int main(void) { // isto é suposto ser ignorado!
+  char i = 'A';
+  while (i <= 'Z')
+  {
+    putchar(i); // ignora pls
+    i = i + 1;
+  }
+  return 0;
+}
